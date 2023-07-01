1539325f915edc992aea825efc41c9fd49789148
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-06-30 23:41:38 -0500

    updated BurderDetailsComponent component jsx code

--- src/burger/BurderDetailsComponent.js
+++ src/burger/BurderDetailsComponent.js
@@ -79,17 +79,18 @@ const BurderDetailsComponent = () => {
 
 	return (
 		<section className="burger-details">
-			<div className="container mt-3 mb-5">
-				{loading ? (
-					<div className="loading">
-						<Loading />
+			<div className="container mt-3">
+				<div className="row ">
+					<div className="col-md-6">
+						<h1 className="text-danger"> {burger.name} Details</h1> <hr />
 					</div>
-				) : error ? (
-					<div className="alert alert-danger text-center">
-						<h5>{error.message}</h5>
+					<div className="col-md-3">
+						<i className="fa fa-chevron-left" />
+						<Link to="/burgers" className="ms-2">
+							Back
+						</Link>
 					</div>
-				) : (
-					<>
+					<div className="col-md-3">
 						<div className="float-end">
 							<div className="cart-items d-flex">
 								<Link to="/shopping-cart">
@@ -106,79 +107,80 @@ const BurderDetailsComponent = () => {
 								</Link>
 							</div>
 						</div>
-						<h1 className="text-danger"> {burger.name} Details</h1> <hr />
-						<div className="row d-flex justify-content-center">
-							<div className="col-md-10">
-								<div className="card">
-									<div className="row">
-										<div className="col-md-6">
-											<div className="images p-3">
-												<div className="text-center p-4">
-													<img src={burger.meal_img} alt={burger.name} />{" "}
-												</div>
-											</div>
-										</div>
-										<div className="col-md-6">
-											<div className="product p-4">
-												<div className="float-end">
-													<i className="fa fa-chevron-left" />
-													<Link to="/burgers" className="ms-2">
-														Back
-													</Link>
-												</div>
-												<div className="mb-3">
-													<h1 className="text-uppercase">{burger.name}</h1>
-
-													<div className="price">
-														{" "}
-														<h2 className="act-price">${burger.price}</h2>
-														<p>
-															<b className="text-warning">Reviews:</b>{" "}
-															{burger.review}
-														</p>
-													</div>
-												</div>
-												<h4 className="text-muted">Description</h4>
-												<p className="about">{burger.description}</p> <hr />
-												<div className="meal_btn text-dark">
-													<span>
-														<i className="fa fa-chevron-right"></i> Reviews:{" "}
-														{burger.review}
-													</span>
-													<span>
-														<i className="fa fa-chevron-right ms-2"></i>{" "}
-														Calories: {burger.calories}
-													</span>
-													<span>
-														<i className="fa fa-chevron-right ms-2"></i> Fiber:{" "}
-														{burger.fiber}
-													</span>
-													<span>
-														<i className="fa fa-chevron-right ms-2"></i>{" "}
-														Protein: {burger.protein}
-													</span>
-													<span>
-														<i className="fa fa-chevron-right ms-2"></i> Carbs:{" "}
-														{burger.carbs}
-													</span>
-												</div>
-												<div className="cart mt-4 align-items-center">
-													<button
-														onClick={(e) => addToCart(e, burger.id)}
-														className="btn btn-outline-warning w-50 btn-lg text-uppercase fw-bold"
-													>
-														ADD TO CART
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</>
-				)}
+					</div>
+				</div>
 			</div>
+			{/* Start of new desing */}
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						{loading ? (
+							<div className="loading">
+								<Loading />
+							</div>
+						) : error ? (
+							<div className="alert alert-danger text-center">
+								<h5>{error.message}</h5>
+							</div>
+						) : (
+							""
+						)}
+					</div>
+				</div>
+				{/* Start of 2 row */}
+				<div className="row shadow-lg p-2 mb-5 bg-body rounded">
+					<div className="col-mg-6 col-md-6 col-sm-12 col-xs-12">
+						<img
+							className="img-fluid left-img"
+							src={burger.meal_img}
+							alt={burger.name}
+						/>
+					</div>
+					<div className="col-mg-6 col-md-6 col-sm-12 col-xs-12">
+						<h1 className="text-uppercase">{burger.name}</h1> <hr />
+						<h2>
+							Price: <b className="text-danger fw-bold">${burger.price}</b>
+						</h2>
+						<p>
+							<b className="text-warning">Reviews:</b> {burger.review}
+						</p>
+						<h4 className="text-muted">Description</h4>
+						<p className="about">{burger.description}</p> <hr />
+						<div className="meal_btn text-dark">
+							<span>
+								<i className="fa fa-chevron-right"></i> Reviews: {burger.review}
+							</span>
+							<span>
+								<i className="fa fa-chevron-right ms-2"></i> Calories:{" "}
+								{burger.calories}
+							</span>
+							<span>
+								<i className="fa fa-chevron-right ms-2"></i> Fiber:{" "}
+								{burger.fiber}
+							</span>
+							<span>
+								<i className="fa fa-chevron-right ms-2"></i> Protein:{" "}
+								{burger.protein}
+							</span>
+							<span>
+								<i className="fa fa-chevron-right ms-2"></i> Carbs:{" "}
+								{burger.carbs}
+							</span>
+						</div>
+						<div className="addToCartBtn mt-4">
+							<button
+								onClick={(e) => addToCart(e, burger.id)}
+								className="btn btn-outline-warning w-100 btn-lg text-uppercase fw-bold"
+							>
+								ADD TO CART
+							</button>
+						</div>
+					</div>
+				</div>
+				{/* End of 2 row */}
+			</div>
+
+			{/* End of new desing */}
 		</section>
 	);
 };
