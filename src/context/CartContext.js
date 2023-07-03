83077f0bc62454f5e121c7198110746c87071c89
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-07-03 12:01:24 -0500

    added cart context

---
+++ src/context/CartContext.js
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react";
+
+//Create a new context
+
+export const CartContext = createContext();
+
+// Create a provider for components to consume and subscribe to changes
+export const CartProvider = ({ children }) => {
+	const [cart, setCart] = useState([]);
+
+	//Add item to the cart
+
+	const addItemToCart = (item) => {
+		setCart((prevCart) => [...prevCart, item]);
+	};
+
+	// Remove item from the cart
+
+	const removeItemFromCart = (itemId) => {
+		setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+	};
+
+	// Clear the cart
+	const clearCart = () => {
+		setCart([]);
+	};
+
+	// Calculate the total quantity of items in the cart
+	const getTotalItems = () => {
+		return cart.reduce((total, item) => total + item.quantity, 0);
+	};
+
+	// Calculate the total price of items in the cart
+	const getTotalPrice = () => {
+		return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+	};
+
+	// Create the context value object
+	const contextValue = {
+		cart,
+		addItemToCart,
+		removeItemFromCart,
+		clearCart,
+		getTotalItems,
+		getTotalPrice,
+	};
+
+	return (
+		<CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+	);
+};
