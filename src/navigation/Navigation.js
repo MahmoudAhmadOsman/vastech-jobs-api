6d0d552797ddfda80b35259507051e4184471dc2
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-08-06 22:27:04 -0500

    updated navigation component

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -5,30 +5,32 @@ import "./NavigationStyle.css";
 const Navigation = () => {
 	return (
 		<div className="site_navigation">
-			<nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<div className="container-fluid">
-					<Link className="navbar-brand " to="/">
+					<Link className="navbar-brand" to="/">
 						VASTECH JOBS
 					</Link>
 					<button
 						className="navbar-toggler"
 						type="button"
 						data-bs-toggle="collapse"
-						data-bs-target="#collapsibleNavbar"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
 					>
 						<span className="navbar-toggler-icon" />
 					</button>
 					<div
-						className="collapse navbar-collapse justify-content-end fw-bold"
-						id="collapsibleNavbar"
+						className="collapse navbar-collapse  "
+						id="navbarSupportedContent"
 					>
-						<ul className="navbar-nav">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 							<li className="nav-item">
 								<Link className="nav-link active" aria-current="page" to="/">
 									Home
 								</Link>
 							</li>
-
 							<li className="nav-item">
 								<Link className="nav-link" to="/jobs">
 									Jobs
